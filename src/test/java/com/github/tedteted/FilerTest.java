076c73ca2bb04226fe82b856a2835668a93403de
Ted Redington <tedtedted>
2021-02-22 00:55:35 +0000

    Initial commit

---
+++ src/test/java/com/github/tedteted/FilerTest.java
@@ -0,0 +1,21 @@
+package com.github.tedteted;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class FilerTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
